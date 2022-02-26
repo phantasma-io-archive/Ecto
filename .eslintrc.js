e599eb21db12ef0b84809c40b3e91074d92e054a
Pau <cryptopvm@gmail.com>
2022-02-26 11:16:36 +0100

    added wallet creation and import using seed words

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    indent: ['off', 4, { SwitchCase: 1 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-empty-function': 'off',
